40e518b8113978fcc5468357b1750fbc980015ef
graham <gpaye8@gmail.com>
2017-11-21 15:09:04 -0800

    rework nav

--- frontend/src/components/layout/layout.jsx
+++ frontend/src/components/layout/layout.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { SocialIcon } from 'react-social-icons';
 
 import './colors.css';
-import './layout.css';
+import './atomics.css';
+import './layout.css'
 
 class Layout extends Component {
   constructor(props) {
@@ -13,14 +14,19 @@ class Layout extends Component {
   render() {
     return (
       <main id="main-wrapper" className="black white-b">
-        <nav className="black-b yellow">
-          <h1>Menu</h1>
-          <h1>Follow Me</h1>
+        <nav>
+          <a className="pill">Menu</a>
+          <a>About</a>
+          <a>Projects</a>
+          <a>Apps</a>
+          <a>Games</a>
+          <a>Skills</a>
+          <a>Get In Touch</a>
           <div className="social-buttons">
-            <SocialIcon className="red-hover" url="https://www.grahampaye.com/" color="#AD5472" />
-            <SocialIcon url="https://www.linkedin.com/in/graham-paye/" color="#AD5472" />
-            <SocialIcon url="https://github.com/TheRoyalTnetennba" color="#AD5472" />
-            <SocialIcon network="email" url="mailto:gpaye8@gmail.com" color="#AD5472" className="cp" />
+            <SocialIcon className="red-hover" url="https://www.grahampaye.com/" color="#eb1478" />
+            <SocialIcon url="https://www.linkedin.com/in/graham-paye/" color="#eb1478" />
+            <SocialIcon url="https://github.com/TheRoyalTnetennba" color="#eb1478" />
+            <SocialIcon network="email" url="mailto:gpaye8@gmail.com" color="#eb1478" className="cp" />
           </div>
         </nav>
 
